db9129da7e02efaf6c41a804b10f525ea8bf1c88
Handell Desulme <handelldesulme@8c-85-90-bc-eb-12.wireless.temple.edu>
2018-11-28 01:25:52 -0500

    updates navigation ids

---
+++ app/src/main/java/com/tuprojects/hd/callalarm/CallListActivity.java
@@ -0,0 +1,6 @@
+package com.tuprojects.hd.callalarm;
+
+import android.support.v7.app.AppCompatActivity;
+
+public class CallListActivity extends AppCompatActivity {
+}
--- app/src/main/java/com/tuprojects/hd/callalarm/MainActivity.java
+++ app/src/main/java/com/tuprojects/hd/callalarm/MainActivity.java
@@ -12,21 +12,21 @@ public class MainActivity extends AppCompatActivity {
     //variables
     private TextView mTextMessage;
 
-    //
+        //variable as anonymous class
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {//listens for clicks
 
         @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {//click handler method
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_contacts:
+                    mTextMessage.setText(R.string.title_contacts);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_call_list:
+                    mTextMessage.setText(R.string.title_call_list);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_history:
+                    mTextMessage.setText(R.string.title_history);
                     return true;
             }
             return false;
@@ -39,8 +39,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
-        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        mTextMessage = (TextView) findViewById(R.id.message); //creates "Contacts" text view (sync w/ layout)
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation); //syncs nav object w/ layout
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
 
